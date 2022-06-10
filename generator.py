47c62b2a1eb73004c8d6992689675b4c24f6c71f
Aaryan Bhurtel <107060344+aaryan66@users.noreply.github.com>
2022-06-10 07:48:32 +0545

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,26 @@
+print("Welcome to the multiplication table generator");
+print("Which multiplication table you want")
+option = int(input(""))
+
+goon = "Yes"
+while goon == "Yes":
+    calculation1  == int(option) * 1
+    calculation2  == int(option) * 2
+    calculation3  == int(option) * 3
+    calculation4  == int(option) * 4
+    calculation5  == int(option) * 5
+    calculation6  == int(option) * 6
+    calculation7  == int(option) * 7
+    calculation8  == int(option) * 8
+    calculation9  == int(option) * 9
+    calculation10  == int(option) * 10
+    print(calculation1)
+    print(calculation2)
+    print(calculation3)
+    print(calculation4)
+    print(calculation5)
+    print(calculation6)
+    print(calculation7)
+    print(calculation8)
+    print(calculation9)
+    print(calculation10)
